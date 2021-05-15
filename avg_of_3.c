6212e07a7bda3375957dea2cd3e534e3fed7a13a
pragunashok <84219009+pragunashok@users.noreply.github.com>
2021-05-15 11:07:47 +0530

    Update avg_of_3.c

--- avg_of_3.c
+++ avg_of_3.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    float a,b,c,avg;
+    float a,b,c,avg;                                            //initialising the three input values, and the average
     printf("Program to find average of three numbers\n");
-    printf("Enter the three numbers");
+    printf("Enter the three numbers");                          //prompt user to enter the three values
     scanf("%f", &a);
     scanf("%f", &b);
-    scanf("%f", &c);
-    avg=(a+b+c)/3;
-    printf("The average of the three numbers is %f", avg);
+    scanf("%f", &c);                                            //accepting user's input
+    avg=(a+b+c)/3;                                              // calculating average
+    printf("The average of the three numbers is %f", avg);      //printing the answer
     return 0;
 }
