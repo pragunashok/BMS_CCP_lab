80aaa53ce2208e5dedbc03dfaf514ee3ba7344f8
pragunashok <84219009+pragunashok@users.noreply.github.com>
2021-07-15 21:17:01 +0530

    Update Fahrenheit to celsius.c

--- Fahrenheit to celsius.c
+++ Fahrenheit to celsius.c
@@ -5,11 +5,11 @@ int main()
 {
 
     float c,f;                                                                   //initialising the variables to store the values
-    printf("This Program Convert degrees Fahrenheit to degrees Celsius \n");
-    printf("Enter The value of degrees Fahrenheit.");                            //prompting the user to enter the value in fahrenheit
+    printf("This Program Convert degrees Fahrenheit to degrees Celsius \n\n");
+    printf("Enter The value of degrees Fahrenheit.    ");                            //prompting the user to enter the value in fahrenheit
     scanf("%f", &f);
     c=(((f-32)*5)/9);                                                            //calculating the corresponding value in celsius
-    printf("The value of %f degrees fahrenheit in degrees celsius is ", f);
-    printf("%fC", c);                                                            //printing the result
+    printf("The value of %.2f degrees fahrenheit in degrees celsius is ", f);
+    printf("%.2fC", c);                                                            //printing the result
     return 0;
 }
